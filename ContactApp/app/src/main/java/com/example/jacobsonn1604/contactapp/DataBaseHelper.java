178979414729c7da909e792165d854c6e7964fc3
n3jacobson <noahchickenjacobson@gmail.com>
2017-05-15 09:31:18 -0700

    Adds additional fields to the app

--- ContactApp/app/src/main/java/com/example/jacobsonn1604/contactapp/DataBaseHelper.java
+++ ContactApp/app/src/main/java/com/example/jacobsonn1604/contactapp/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.jacobsonn1604.contactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,6 +14,8 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "Contact_Table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
+    public static final String COL_3 = "Address";
+    public static final String COL_4 = "Age";
 
 
     public DataBaseHelper(Context context) {
@@ -21,12 +24,25 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, AGE TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String address, String age){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3,address);
+        contentValues.put(COL_4,age);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }
+        else return true;
     }
 }
--- ContactApp/app/src/main/java/com/example/jacobsonn1604/contactapp/MainActivity.java
+++ ContactApp/app/src/main/java/com/example/jacobsonn1604/contactapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.jacobsonn1604.contactapp;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity{
 
     DataBaseHelper myDb;
     EditText editName;
+    EditText editAddress;
+    EditText editAge;
     Button btnAddData;
 
     @Override
@@ -21,5 +25,22 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+        editAge = (EditText) findViewById(R.id.editText_age);
+
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(),editAddress.getText().toString(),editAge.getText().toString());
+        if(isInserted == true){
+            //Create toast message to user indicating data inserted correctly
+            Log.d("MyContact","Data insertion successful");
+        }
+        else{
+            //Create toast message to user indicating data inserted incorrectly
+            Log.d("MyContact", "Data insertion NOT successful");
+        }
     }
 }
